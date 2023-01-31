399307a654880374f0a775a2bd18ea66941efb5c
androide91_home <andrade.1905@hotmail.com>
2023-01-30 22:11:45 -0600

    delete

--- oem/webpack.mix.js
+++
@@ -1,6 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('src/app.js', 'dist').setPublicPath('dist');
-mix.setResourceRoot(process.env.MIX_RESOURCE_ROOT);
-mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts')
-   .sass('resources/assets/sass/app.scss', 'public/css');
